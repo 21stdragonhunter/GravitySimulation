ddedb9817c141d04691d8b1c84993e2ad4deac09
Coleman Smith <csmitt23@gmail.com>
2016-01-20 11:16:55 -0500

    finished collide method of Particle

--- src/js/main.js
+++ src/js/main.js
@@ -36,8 +36,20 @@ function canvasApp() {
         var run = other.y - this.y;
         this.reflect(run, rise);
         other.reflect(run, rise);
-        
+        this_velocity = ((this.velocity * this.mass) + (other.velocity * other.mass) +
+                        ((1 - MOMENTUM_TRANSFER_RATE) * Math.abs((this.velocity * this.mass) - (other.velocity * other.mass))))
+                        / (2 * this.mass);
+        other_velocity = ((this.mass * this_velocity) -
+                         (1 - MOMENTUM_TRANSFER_RATE) * Math.abs((this.velocity * this.mass) - (other.velocity * other.mass)))
+                         / other.mass;
+        this.velocity = this_velocity;
+        other.velocity = other_velocity;
     };
+    Particle.prototype.move = function() {
+        this.vector.nextPosition();
+        // check for collisions with other particles, and then call collision if they have collided. The methodneeds to be
+        // reworked in case more than two particles collide at once
+    }
 
 
 
